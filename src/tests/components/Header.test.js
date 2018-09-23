dc9db4bc65f90c855bdb755d6633d463fc7b1d98
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-09-23 06:38:55 -0400

    Add expense summary to dashboard

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -6,13 +6,3 @@ test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
 });
-
-
-
-
-
-  // expect(wrapper.find('h1').text()).toBe("Expensify");
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
-  // console.log(renderer.getRenderOutput());
